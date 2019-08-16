162496bfe76b9d4fa771d2729ba1837330fd593f
Blake Neely <blakeneely21@gmail.com>
2019-08-15 22:57:06 -0500

    attack button deducts hp

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,130 +1,185 @@
 $(document).ready(function() {
-var hero = 0;
-var enemies = 3;
-var heroSelected = false;
-var enemySelected = false;
-var starkHero = false;
-var starkEnemy = false;
-var baratheonHero = false;
-var baratheonEnemy = false;
-var targaryenHero = false;
-var targaryenEnemy = false;
-var lannisterHero = false;
-var lannisterEnemy = false;
+    var heroCount = 0;
+    var enemiesCount = 3;
+    var heroSelected = false;
+    var enemySelected = false;
+    var hero = {};
+    var enemy = {};
+    var ogAttack = 0;
+    // var starkHero = false;
+    // var starkEnemy = false;
+    // var baratheonHero = false;
+    // var baratheonEnemy = false;
+    // var targaryenHero = false;
+    // var targaryenEnemy = false;
+    // var lannisterHero = false;
+    // var lannisterEnemy = false;
 
-var stark = {
-    name: "Stark",
-    hp: 120,
-    attack: 12,
-    counter: 10
-};
+    var characters = {
+        "Stark": {
+            name: "Stark",
+            hp: 120,
+            attack: 8,
+            counter: 15
+        },
+        "Baratheon": {
+            name: "Baratheon",
+            hp: 100,
+            attack: 14,
+            counter: 5
+        },
+        "Targaryen": {
+            name: "Targaryen",
+            hp: 150,
+            attack: 8,
+            counter: 5
+        },
+        "Lannister": {
+            name: "Lannister",
+            hp: 180,
+            attack: 7,
+            counter: 25
+    }};
 
-var baratheon = {
-    name: "Baratheon",
-    hp: 100,
-    attack: 10,
-    counter: 8
-};
+    function gameRestart() {
+        
+    };
 
-var targaryen = {
-    name: "Targaryen",
-    hp: 150,
-    attack: 15,
-    counter: 12
-};
+    function instructionsText(){
+        if (!enemySelected){
+            $("#instructions").text("Now chose an enemy!");
+        }
+        else if (heroSelected && enemySelected) {
+            $("#instructions").text("ATTACK!!!")
+        }
+    };
 
-var lannister = {
-    name: "Lannister",
-    hp: 180,
-    attack: 18,
-    counter: 15
-};
+    function fight() {
+        hero.hp = hero.hp - enemy.counter;
+        enemy.hp = enemy.hp - hero.attack;
+        hero.attack =  hero.attack + ogAttack;
+        console.log("Hero hp: " + hero.hp);
+        console.log("Hero attack: " + hero.attack);
+        console.log("Enemy hp: " + enemy.hp);
+        // deduct hero attack # from enemy hp
+        // deduct enemy counter # from hero hp
+    };
 
-function gameStart() {
-    
-};
+    function roundOver() {
+        if (hero.hp === 0) {
+            alert("You lose. Game Over!")
+        }
+        else if (enemy.hp === 0) {
+            // hide enemy card
+            // change text to choose new enemy
+            // change text to attack again
 
-function instructionsText(){
-    if (!enemySelected){
-        $("#instructions").text("Now chose an enemy!");
-    }
-    else if (heroSelected && enemySelected) {
-        $("#instructions").text("ATTACK!!!")
-    }
-};
+        }
+    };
 
-// On click hide main card and show hero or enemy card
-$("#stark").on("click", function(){
-    if (!heroSelected){
-        $(this).toggleClass("hidden");
-        $("#hero-stark").toggleClass("hidden");
-        heroSelected = true;
-        instructionsText();
-    }
-    else if (heroSelected && enemySelected) {
-        return false;
-    }
-    else {
-        $(this).toggleClass("hidden");
-        $("#enemy-stark").toggleClass("hidden");
-        enemySelected = true;  
-        instructionsText();  
-    }
-});
+    // On click hide main card and show hero or enemy card
+    $("#stark").on("click", function(){
+        if (!heroSelected){
+            $(this).toggleClass("hidden");
+            $("#hero-stark").toggleClass("hidden");
+            hero = characters["Stark"];
+            ogAttack = hero.attack;
+            console.log(hero);
+            heroSelected = true;
+            instructionsText();
+        }
+        else if (heroSelected && enemySelected) {
+            return false;
+        }
+        else {
+            $(this).toggleClass("hidden");
+            $("#enemy-stark").toggleClass("hidden");
+            enemy = characters["Stark"];
+            console.log(enemy);
+            enemySelected = true;  
+            instructionsText();  
+        }
+    });
 
-$("#baratheon").on("click", function(){
-    if (!heroSelected) {
-        $(this).toggleClass("hidden");
-        $("#hero-baratheon").toggleClass("hidden");
-        heroSelected = true;
-        instructionsText();
-    }
-    else if (heroSelected && enemySelected) {
-        return false;
-    }
-    else {
-        $(this).toggleClass("hidden");
-        $("#enemy-baratheon").toggleClass("hidden");
-        enemySelected = true;
-        instructionsText();
-    }
-});
+    $("#baratheon").on("click", function(){
+        if (!heroSelected) {
+            $(this).toggleClass("hidden");
+            $("#hero-baratheon").toggleClass("hidden");
+            hero = characters["Baratheon"];
+            ogAttack = hero.attack;
+            console.log(hero);
+            heroSelected = true;
+            instructionsText();
+        }
+        else if (heroSelected && enemySelected) {
+            return false;
+        }
+        else {
+            $(this).toggleClass("hidden");
+            $("#enemy-baratheon").toggleClass("hidden");
+            enemy = characters["Baratheon"];
+            console.log(enemy);
+            enemySelected = true;
+            instructionsText();
+        }
+    });
 
-$("#targaryen").on("click", function(){
-    if (!heroSelected) {
-        $(this).toggleClass("hidden");
-        $("#hero-targaryen").toggleClass("hidden");
-        heroSelected = true;
-        instructionsText();
-    }
-    else if (heroSelected && enemySelected) {
-        return false;
-    }
-    else {
-        $(this).toggleClass("hidden");
-        $("#enemy-targaryen").toggleClass("hidden");
-        enemySelected = true;
-        instructionsText();
-    }
-});
+    $("#targaryen").on("click", function(){
+        if (!heroSelected) {
+            $(this).toggleClass("hidden");
+            $("#hero-targaryen").toggleClass("hidden");
+            hero = characters["Targaryen"];
+            ogAttack = hero.attack;
+            console.log(hero);
+            heroSelected = true;
+            instructionsText();
+        }
+        else if (heroSelected && enemySelected) {
+            return false;
+        }
+        else {
+            $(this).toggleClass("hidden");
+            $("#enemy-targaryen").toggleClass("hidden");
+            enemy = characters["Targaryen"];
+            console.log(enemy);
+            enemySelected = true;
+            instructionsText();
+        }
+    });
 
-$("#lannister").on("click", function(){
-    if (!heroSelected) {
-        $(this).toggleClass("hidden");
-        $("#hero-lannister").toggleClass("hidden");
-        heroSelected = true;
-        instructionsText();
-    }
-    else if (heroSelected && enemySelected) {
-        return false;
-    }
-    else {
-        $(this).toggleClass("hidden");
-        $("#enemy-lannister").toggleClass("hidden");
-        enemySelected = true;
-        instructionsText();
-    }
-});
+    $("#lannister").on("click", function(){
+        if (!heroSelected) {
+            $(this).toggleClass("hidden");
+            $("#hero-lannister").toggleClass("hidden");
+            hero = characters["Lannister"];
+            ogAttack = hero.attack;
+            console.log(hero);
+            heroSelected = true;
+            instructionsText();
+        }
+        else if (heroSelected && enemySelected) {
+            return false;
+        }
+        else {
+            $(this).toggleClass("hidden");
+            $("#enemy-lannister").toggleClass("hidden");
+            enemy = characters["Lannister"];
+            console.log(enemy);
+            enemySelected = true;
+            instructionsText();
+        }
+    });
+
+    // On click attack button deduct hp points
+    $(".button").on("click", function(){
+        if (heroSelected && enemySelected) {
+            fight();
+        }
+        else {
+            return false;
+        }
+        
+    });
 
 
 
