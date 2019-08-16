87444efd428cc1693eed7c5f9b09beccee09f127
Blake Neely <blakeneely21@gmail.com>
2019-08-16 13:37:55 -0500

    core game is finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,61 +12,60 @@ $(document).ready(function() {
             name: "Stark",
             hp: 120,
             attack: 8,
-            counter: 15
+            counter: 15,
+            hpHtml: ".stark-hp"
         },
         "Baratheon": {
             name: "Baratheon",
             hp: 100,
             attack: 14,
-            counter: 5
+            counter: 5,
+            hpHtml: ".baratheon-hp"
         },
         "Targaryen": {
             name: "Targaryen",
             hp: 150,
             attack: 8,
-            counter: 5
+            counter: 5,
+            hpHtml: ".targaryen-hp"
         },
         "Lannister": {
             name: "Lannister",
             hp: 180,
             attack: 7,
-            counter: 25
+            counter: 25,
+            hpHtml: ".lannister-hp"
     }};
-
-    function gameRestart() {
-        // reset variables
-        // hide enemy and hero cards
-        // 
-    };
 
     function instructionsText(){
         if (!enemySelected){
             $("#instructions").text("Now chose an enemy!");
         }
         else if (heroSelected && enemySelected) {
-            $("#instructions").text("ATTACK!!!")
+            $("#instructions").text("ATTACK!!!");
         }
     };
 
     function fight() {
-        hero.hp = hero.hp - enemy.counter;      // deduct hero attack # from enemy hp
-        enemy.hp = enemy.hp - hero.attack;      // deduct enemy counter # from hero hp
-        hero.attack =  hero.attack + ogAttack;  // add original hero attack to itself
-        roundOver();
+        hero.hp = hero.hp - enemy.counter;              // deduct hero attack # from enemy hp
+        enemy.hp = enemy.hp - hero.attack;              // deduct enemy counter # from hero hp
+        $(enemy.hpHtml).text(enemy.hp);
+        $(hero.hpHtml).text(hero.hp);
         $("#hero-text").text("You attacked House " + enemy.name + " for " + hero.attack + " damage!");
         $("#enemy-text").text("House " + enemy.name + " countered with " + enemy.attack + " damage");
-        console.log("Hero hp: " + hero.hp);
-        console.log("Hero attack: " + hero.attack);
-        console.log("Enemy hp: " + enemy.hp);
+        roundOver();
+        hero.attack =  hero.attack + ogAttack;          // add original hero attack to itself
     };
 
     function roundOver() {
         if (hero.hp <= 0) {
-            alert("You lose. Game Over!")
+            $("#instructions").text("You were defeated. Game over.")
+            gameOver();
         }
         else if (enemy.hp <= 0) {
             enemiesCount--;
-            alert("bang");
+            $("#hero-text").text("You defeated House " + enemy.name + "!");
+            $("#enemy-text").text(" ");
             // hide enemy card
             if (enemy === characters["Stark"]) {
                 $("#enemy-stark").toggleClass("hidden");
@@ -82,32 +81,39 @@ $(document).ready(function() {
             };
             $("#instructions").text("CHOOSE A NEW ENEMY");
             enemySelected = false;
-            $("#hero-text").text(" ");
-            $("#enemy-text").text(" ")
+            gameOver();
         }
     };
 
-    // On click hide main card and show hero or enemy card
-    $("#stark").on("click", function(){
-        if (!heroSelected){
-            $(this).toggleClass("hidden");
-            $("#hero-stark").toggleClass("hidden");
-            hero = characters["Stark"];
-            ogAttack = hero.attack;
-            console.log(hero);
-            heroSelected = true;
-            instructionsText();
+    function gameOver() {
+        if (enemiesCount === 0) {
+            $(".attack-button").toggleClass("hidden");
+            $(".restart-button").toggleClass("hidden");
+            $("#instructions").text("YOU WON THE THRONE!!");
         }
-        else if (heroSelected && enemySelected) {
+    };
+
+    // On click character hide main card and show hero or enemy card
+    $("#stark").on("click", function(){
+        if (!heroSelected){                                 // Checks if hero has been selected yet
+            $(this).toggleClass("hidden");                  // Hides main card
+            $("#hero-stark").toggleClass("hidden");         // Shows matching hero card
+            hero = characters["Stark"];                     // Assigns clicked object to variable hero
+            ogAttack = hero.attack;                         // Assigns attack value to ogAttack for adding later
+            console.log(hero);
+            heroSelected = true;                            // Flags that hero has been selected
+            instructionsText();                             // Updates instructions to choose enemy
+        }
+        else if (heroSelected && enemySelected) {           // If both hero and enemy selected stops click
             return false;
         }
         else {
-            $(this).toggleClass("hidden");
-            $("#enemy-stark").toggleClass("hidden");
-            enemy = characters["Stark"];
+            $(this).toggleClass("hidden");                  // Hides main card
+            $("#enemy-stark").toggleClass("hidden");        // Shows matching enemy card
+            enemy = characters["Stark"];                    // Assigns clicked object to variable enemy
             console.log(enemy);
-            enemySelected = true;  
-            instructionsText();  
+            enemySelected = true;                           // Flags that enemy has been selected
+            instructionsText();                             // Updates instructions to attack
         }
     });
 
@@ -181,15 +187,19 @@ $(document).ready(function() {
     });
 
     // On click attack button deduct hp points
-    $(".button").on("click", function(){
+    $(".attack-button").on("click", function(){
         if (heroSelected && enemySelected) {
             fight();
         }
         else {
             return false;
         }
-        // $("#hero-text").text("You attacked House " + enemy.name + " for " + hero.attack + " damage!");
-        // $("#enemy-text").text("House " + enemy.name + " countered with " + enemy.attack + " damage");
+    });
+
+    // On click restart button
+    $(".restart-button").on("click", function(){
+        location.reload();
+
     });
 
 
